d8678f355d55a98ed9e4b9635f4fd801cd6cf639
axeldamian <axel.arrondo@mercadolibre.com>
2024-11-15 22:52:14 -0300

    controller para construir un robot random hecho

---
+++ robotounament/src/main/java/com/torneo/robotounament/controllers/RobotsController.java
@@ -0,0 +1,25 @@
+package com.torneo.robotounament.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.torneo.robotounament.services.BuildRobot;
+
+@RestController
+public class RobotsController {
+
+        @Autowired
+        private BuildRobot service;
+
+        @GetMapping(value = "/build")
+        public ResponseEntity<String> build() {
+
+                service.ramdomComponents();
+
+                return new ResponseEntity<String>("Robot random created successfully", HttpStatus.OK);
+        }
+    
+}
