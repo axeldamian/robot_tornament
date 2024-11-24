6cc436efbfe94a751c46aef579a737911e72936d
axeldamian <axel.arrondo@mercadolibre.com>
2024-11-24 19:45:59 -0300

    agregue cosas

--- robotounament/src/main/java/com/torneo/robotounament/objects/Piloto.java
+++ robotounament/src/main/java/com/torneo/robotounament/objects/Piloto.java
@@ -15,5 +15,65 @@ public class Piloto {
     private String name;
 
     private int creativity;
+
+    private int brave;
+
+    private int experience;
+
+    private int battlesWon;
+
+    private int battlesLose;
+
+    private int championshipsWon;
+
+    private boolean penalized;
+
+    private int datesPenalized;
+
+    private boolean duelBeforeTheBattle;
+
+    public Piloto() {
+        this.experience = 0;
+        this.penalized = false;
+        this.datesPenalized = 0;
+        this.duelBeforeTheBattle = false;
+        this.battlesLose = 0;
+        this.battlesWon = 0;
+        this.championshipsWon = 0;
+    }
+
+    public void toPenalize(int days){
+        this.penalized = true;
+    }
+
+    public int getAgility() {
+        return this.agility - this.age + this.experience;
+    }
+
+    public void incremenBattlesWon(){
+        this.battlesWon = this.battlesWon + 1;
+    }
+
+    public int getBattlesWon() {
+        return this.battlesWon;
+    }
+
+    public int getBattlesLose(){
+        return this.battlesLose;
+    }
+
+    public void incrementBattlesLose(){
+        this.battlesLose = this.battlesLose + 1;
+    }
+
+    public void incrementChampionshipsWon(){
+        this.championshipsWon = this.championshipsWon + 1;
+    }
+
+    public int getChampionshipsWon(){
+        return this.championshipsWon;
+    }
+
+
     
 }
