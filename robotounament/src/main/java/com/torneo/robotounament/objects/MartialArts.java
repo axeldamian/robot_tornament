3ddf220f69ea5dad181ce2f621a034142aec8df5
axeldamian <axel.arrondo@mercadolibre.com>
2024-11-24 16:28:37 -0300

    changes

---
+++ robotounament/src/main/java/com/torneo/robotounament/objects/MartialArts.java
@@ -0,0 +1,11 @@
+package com.torneo.robotounament.objects;
+
+public enum MartialArts {
+
+    MartialArts(int energy, int power){
+        this.energy = energy;
+        this.power = power;
+        this.works = true;
+    }
+    
+}
---
+++ robotounament/src/main/java/com/torneo/robotounament/objects/Piloto.java
@@ -0,0 +1,19 @@
+package com.torneo.robotounament.objects;
+
+public class Piloto {
+
+    private int agility;
+
+    private boolean defensive;
+
+    private boolean attacker;
+
+    private int age;
+
+    private int weight;
+
+    private String name;
+
+    private int creativity;
+    
+}
